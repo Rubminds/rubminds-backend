540250ff126ee9f1331e9c94709abe10dded369a
jy-agnes-lee <jy.agnes.lee@gmail.com>
2021-11-18 19:20:49 +0900

    feat: 회원정보 조회 생성(#5)

--- src/main/java/com/rubminds/api/user/service/UserService.java
+++ src/main/java/com/rubminds/api/user/service/UserService.java
@@ -4,6 +4,8 @@ import com.rubminds.api.user.domain.User;
 import com.rubminds.api.user.domain.repository.UserRepository;
 import com.rubminds.api.user.dto.AuthRequest;
 import com.rubminds.api.user.dto.AuthResponse;
+import com.rubminds.api.user.dto.UserRequest;
+import com.rubminds.api.user.dto.UserResponse;
 import com.rubminds.api.user.exception.DuplicateNicknameException;
 import com.rubminds.api.user.exception.UserNotFoundException;
 import com.rubminds.api.user.security.userdetails.CurrentUser;
@@ -26,6 +28,15 @@ public class UserService {
         }
         user.update(request);
         return AuthResponse.Signup.build(user.getId(), user.getNickname(), user.getJob(), user.getIntroduce() ,user.isSignupCheck());
+    }
+
+    public UserResponse.Info mypage(@CurrentUser CustomUserDetails customUserDetails){
+        User user = userRepository.findById(customUserDetails.getUser().getId()).orElseThrow(UserNotFoundException::new);
+        return UserResponse.Info.build(user.getId(), user.getNickname(), user.getJob(), user.getIntroduce());
+    }
+    public UserResponse.Info info(UserRequest.Info request){
+        User user = userRepository.findById(request.getId()).orElseThrow(UserNotFoundException::new);
+        return UserResponse.Info.build(user.getId(), user.getNickname(), user.getJob(), user.getIntroduce());
     }
 
 
--- src/main/java/com/rubminds/api/user/web/UserController.java
+++ src/main/java/com/rubminds/api/user/web/UserController.java
@@ -2,15 +2,14 @@ package com.rubminds.api.user.web;
 
 import com.rubminds.api.user.dto.AuthRequest;
 import com.rubminds.api.user.dto.AuthResponse;
+import com.rubminds.api.user.dto.UserRequest;
+import com.rubminds.api.user.dto.UserResponse;
 import com.rubminds.api.user.security.userdetails.CurrentUser;
 import com.rubminds.api.user.security.userdetails.CustomUserDetails;
 import com.rubminds.api.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -26,4 +25,15 @@ public class UserController {
         return ResponseEntity.ok().body(signupResponse);
     }
 
+    @GetMapping("/mypage")
+    public ResponseEntity<UserResponse.Info> mypage(@CurrentUser CustomUserDetails customUserDetails, HttpServletResponse response){
+        UserResponse.Info mypageResponse = userService.mypage(customUserDetails);
+        return ResponseEntity.ok().body(mypageResponse);
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<UserResponse.Info> info(@RequestBody UserRequest.Info request, HttpServletResponse response){
+        UserResponse.Info infoResponse = userService.info(request);
+        return ResponseEntity.ok().body(infoResponse);
+    }
 }

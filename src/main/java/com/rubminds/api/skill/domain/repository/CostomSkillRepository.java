861d08cb2ec66e11e13ca38eca7b2077e5a495f6
kikiplus1 <kikiplus1@naver.com>
2021-12-07 00:24:44 +0900

    feat : CostomSkill 도매인 및 db생성

---
+++ src/main/java/com/rubminds/api/skill/domain/repository/CostomSkillRepository.java
@@ -0,0 +1,14 @@
+package com.rubminds.api.skill.domain.repository;
+
+import com.rubminds.api.post.domain.Post;
+import com.rubminds.api.skill.domain.CostomSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CostomSkillRepository extends JpaRepository<CostomSkill, Long> {
+        Optional<CostomSkill> findById(Long id);
+        List<CostomSkill> findAllByPost(Post post);
+        List<CostomSkill> deleteAllByPost(Post post);
+}

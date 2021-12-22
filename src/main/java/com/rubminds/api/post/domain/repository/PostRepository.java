93fd774698ec4e977a7d392e92fbc97349c3ebf4
donup3 <donup08@naver.com>
2021-12-22 20:49:50 +0900

    fix: gradle 버전에 맞춰 restdocs 설정 수정(#48)

--- src/main/java/com/rubminds/api/post/domain/repository/PostRepository.java
+++ src/main/java/com/rubminds/api/post/domain/repository/PostRepository.java
@@ -2,9 +2,6 @@ package com.rubminds.api.post.domain.repository;
 
 import com.rubminds.api.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 

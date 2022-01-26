fe24c34f98d21f0ada137d15481a6a9a16af4c10
jy-agnes-lee <jy.agnes.lee@gmail.com>
2022-01-26 20:45:29 +0900

    feat: 게시물 삭제 기능 수정 (#109)

--- src/main/java/com/rubminds/api/user/domain/repository/UserRepository.java
+++ src/main/java/com/rubminds/api/user/domain/repository/UserRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface
-UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByNickname(String nickname);
 
     Optional<User> findByOauthId(String oauthId);
